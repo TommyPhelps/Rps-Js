1a664e61d1dd9ea6110d81bc8c9a79df5ea2c973
Tommy Phelps <tommydustinphelps@gmail.com>
2024-04-01 22:18:51 -0500

    Add function to get the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello")
+function getComputerChoice() {
+    choice = ["rock", "paper", "scissors"];
+    num = 0;
+    result = "";
+    return choice[Math.floor(Math.random()*3)];
+}
+
+console.log(getComputerChoice());
